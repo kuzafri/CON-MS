8c0bb6c336198ab3ba40625b9e55fd5d2702889f
Kuzafri <64196366+kuzafri@users.noreply.github.com>
2024-11-10 22:33:22 +0800

    add theme

--- src/main.js
+++ src/main.js
@@ -2,4 +2,30 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
+import ConfirmationService from 'primevue/confirmationservice'
+import ToastService from 'primevue/toastservice'
+
+// import '@/assets/styles.scss'
+// import '@/assets/tailwind.css'
+
+const app = createApp(App)
+
+
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        options: {
+            darkModeSelector: '.app-dark'
+        }
+    }
+})
+
+// Add PrimeVue services
+app.use(ToastService)
+app.use(ConfirmationService)
+
+// Mount the app
+app.mount('#app')
