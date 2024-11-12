f27c95950bed5feddee3a44bcc545b2ae48e02f6
Kuzafri <64196366+kuzafri@users.noreply.github.com>
2024-11-12 17:51:20 +0800

    add dashboard and resetup firebase

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-# Vue 3 + Vite
+Firebase Reference
 
-This template should help get you started developing with Vue 3 in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+https://medium.com/@emperorbrains/vue-js-and-firebase-building-real-time-applications-8c8db628ee31
 
-Learn more about IDE Support for Vue in the [Vue Docs Scaling up Guide](https://vuejs.org/guide/scaling-up/tooling.html#ide-support).
---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { getAnalytics } from 'firebase/analytics';
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: 'AIzaSyAidRzfccsCj_p_Qb2ncQEigBQ7jU-53uQ',
+    authDomain: 'con-ms.firebaseapp.com',
+    projectId: 'con-ms',
+    storageBucket: 'con-ms.firebasestorage.app',
+    messagingSenderId: '1009529497926',
+    appId: '1:1009529497926:web:8fd1e9cfc1c01cdc30dd47',
+    measurementId: 'G-QGVKFW4L3R'
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
