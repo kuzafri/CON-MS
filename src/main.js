ae6014d3dc32659d5ac27f156a53e2ba1807b617
Kuzafri <64196366+kuzafri@users.noreply.github.com>
2024-11-12 09:40:05 +0800

    replace src

--- src/main.js
+++ src/main.js
@@ -1,19 +1,18 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
 
+import Aura from '@primevue/themes/aura';
+import PrimeVue from 'primevue/config';
+import ConfirmationService from 'primevue/confirmationservice';
+import ToastService from 'primevue/toastservice';
 
-import PrimeVue from 'primevue/config'
-import Aura from '@primevue/themes/aura'
-import ConfirmationService from 'primevue/confirmationservice'
-import ToastService from 'primevue/toastservice'
+import '@/assets/styles.scss';
+import '@/assets/tailwind.css';
 
-// import '@/assets/styles.scss'
-// import '@/assets/tailwind.css'
+const app = createApp(App);
 
-const app = createApp(App)
-
-
+app.use(router);
 app.use(PrimeVue, {
     theme: {
         preset: Aura,
@@ -21,11 +20,8 @@ app.use(PrimeVue, {
             darkModeSelector: '.app-dark'
         }
     }
-})
+});
+app.use(ToastService);
+app.use(ConfirmationService);
 
-// Add PrimeVue services
-app.use(ToastService)
-app.use(ConfirmationService)
-
-// Mount the app
-app.mount('#app')
+app.mount('#app');
