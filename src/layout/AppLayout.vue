fdcec4d96fa0770d86945fdcbe6e1870fc420b34
Kuzafri <64196366+kuzafri@users.noreply.github.com>
2024-12-04 17:01:24 +0800

    applayout conflict merge solve

--- src/layout/AppLayout.vue
+++ src/layout/AppLayout.vue
@@ -1,9 +1,6 @@
 <script setup>
 import { useLayout } from '@/layout/composables/layout';
 import { computed, ref, watch } from 'vue';
-import AppFooter from './AppFooter.vue';
-import AppSidebar from './AppSidebar.vue';
-import AppTopbar from './AppTopbar.vue';
 
 const { layoutConfig, layoutState, isSidebarActive, resetMenu } = useLayout();
 
@@ -55,9 +52,8 @@ function isOutsideClicked(event) {
 
 <template>
     <div class="layout-wrapper" :class="containerClass">
-            <div class="layout-main">
-                <router-view></router-view>
-            </div>
+        <div class="layout-main">
+            <router-view></router-view>
         </div>
         <div class="layout-mask animate-fadein"></div>
     </div>
